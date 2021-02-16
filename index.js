e28daa98f2066d5e2d92d6e903e4cb39e6016aa2
danielstl <danielstl@github.io>
2021-02-16 16:12:01 +0000

    additional data validation, and doodle support

--- index.js
+++ index.js
@@ -48,28 +48,15 @@ app.use(history({
         }
     ],
     verbose: true
-}))
+}));
 
 const mongoClient = require("mongodb").MongoClient;
 const databaseUrl = "mongodb://localhost:27017/";
 
 mongoClient.connect(databaseUrl).then(db => {
-//app.use(cors());
-
-//console.log(db);
 
     let dbo = db.db("smartdoor");
     let displays = dbo.collection("displays");
-
-    app.get("/fetch-data/:id", async (req, res) => {
-        let id = req.params.id;
-
-        let doc = await displays.findOne({"_id": id});
-
-        res.status(200).json(doc);
-
-        //await displays.insertOne({"_id": "TEST", "test1": "test2"});
-    });
 
     const storage = multer.diskStorage({
         destination: function (req, file, callback) {
@@ -94,7 +81,7 @@ mongoClient.connect(databaseUrl).then(db => {
 
         console.log("Saved image: " + path);
 
-        io.emit("background_update", path);
+        io.emit("background_update", URL + path);
         res.status(200).redirect("/manage/customise"); //todo temporary
 
         await displays.updateOne({"roomId": req.params.roomId}, {"$set": {"backgroundUrl": URL + path}});
@@ -117,6 +104,17 @@ mongoClient.connect(databaseUrl).then(db => {
         io.emit("user_update", doc.value.user);
     });
 
+    app.post("/upload/note-image/:roomId", upload.single("image"), async (req, res) => {
+        if (!req.file) {
+            res.status(400).json({error: "No image"});
+            return;
+        }
+
+        let path = URL + "images/" + req.file.filename;
+
+        res.status(200).json({url: path});
+    });
+
     let clientRooms = {}; //will be a map of clients -> room code
 
     async function getActiveDisplay(client) {
@@ -131,8 +129,13 @@ mongoClient.connect(databaseUrl).then(db => {
         next(); //TODO: middleware for authentication if needed goes here https://socket.io/docs/v3/middlewares/
     });
 
-    function registerClientEvents(client, enableManagementEvents) {
+    function registerClientEvents(client, enableManagementEvents, newPairing) {
         client.join(clientRooms[client]); //join them to the room for their room id
+        console.log("joined client to room " + clientRooms[client]);
+
+        if (newPairing) {
+            io.in(clientRooms[client]).emit("new_device_joined");
+        }
 
         client.on("get_room_id", async () => {
             client.emit("room_id", clientRooms[client]);
@@ -156,6 +159,15 @@ mongoClient.connect(databaseUrl).then(db => {
             client.emit("background_update", doc.backgroundUrl);
         });
 
+        client.on("get_widgets", async () => {
+            let doc = await getActiveDisplay(client);
+            if (doc === null) {
+                return;
+            }
+
+            client.emit("widgets_update", doc.widgets);
+        });
+
         client.on("get_notes", async () => {
             let doc = await getActiveDisplay(client);
             if (doc === null) {
@@ -165,35 +177,80 @@ mongoClient.connect(databaseUrl).then(db => {
             client.emit("notes_update", doc.notes);
         });
 
+        client.on("get_doodles", async () => {
+            let doc = await getActiveDisplay(client);
+            if (doc === null) {
+                return;
+            }
+
+            client.emit("doodles_update", doc.doodles || []);
+        });
+
         client.on("get_calendar", async () => {
             let doc = await getActiveDisplay(client);
             if (doc === null) {
                 return;
             }
 
-            if (doc.calendarUrl) {
-                let url = doc.calendarUrl;
-                let data = await getCalendarData(url);
-                io.emit("calendar_update", {url, data});
-            } else {
-                io.emit("calendar_update", null);
+            let eventData = null;
+
+            if (doc.calendars) {
+                eventData = await getAllCalendarData(doc.calendars);
             }
+
+            io.in(clientRooms[client]).emit("calendar_update", eventData);
         });
 
-        client.on("send_message", async messageData => {
-            io.emit("new_message", messageData);
+        client.on("send_message", async message => {
+            if (!message || message === "") {
+                return;
+            }
+
+            let messageObj = {
+                content: message,
+                fromSystem: enableManagementEvents,
+                timestamp: new Date().getTime()
+            }
+            io.in(clientRooms[client]).emit("new_message", messageObj);
         });
 
         //video chat
 
-        client.on("start_intercom_call", sdp => {
-            console.log("CALL!", sdp);
-            client.broadcast.emit("intercom_call_signalling", {message: "sdp", sdp});
+        client.on("start_intercom_call", requestId => {
+            if (requestId === undefined) {
+                return;
+            }
+
+            io.in(clientRooms[client]).emit("intercom_call_request", requestId.toString());
+            //console.log("CALL!", sdp);
+            //client.broadcast.emit("intercom_call_signalling", {message: "sdp", sdp});
+        });
+
+        client.on("decline_call_request", requestId => {
+            if (!requestId) {
+                return;
+            }
+
+            io.in(clientRooms[client]).emit("decline_call_request", requestId.toString());
+        });
+
+        client.on("end_intercom_call", requestId => {
+            if (!requestId) {
+                return;
+            }
+
+            io.in(clientRooms[client]).emit("end_intercom_call", requestId.toString());
         });
 
         client.on("intercom_call_signalling", data => {
             console.log("intercom signalling!", data);
             client.broadcast.emit("intercom_call_signalling", data);
+        });
+
+        client.on("add_doodle", async doodleUrl => {
+            let doc = await findAndUpdateActiveDisplay(client, {"$push": {"doodles": doodleUrl.toString()}}, {returnOriginal: false});
+
+            io.in(clientRooms[client]).emit("doodles_update", doc.value.doodles);
         });
 
         if (!enableManagementEvents) return; //don't register management events
@@ -224,38 +281,122 @@ mongoClient.connect(databaseUrl).then(db => {
             }
         });
 
-        client.on("update_status", async status => {
-            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"user.status": status}}, {returnOriginal: false});
+        client.on("remove_doodle", async doodleUrl => {
+            let doc = await findAndUpdateActiveDisplay(client, {"$pull": {"doodles": doodleUrl.toString()}}, {returnOriginal: false});
 
-            io.emit("user_update", doc.value.user); //TODO emit only to room
+            io.in(clientRooms[client]).emit("doodles_update", doc.value.doodles);
+        });
+
+        client.on("update_status", async status => {
+            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"user.status": status.toString()}}, {returnOriginal: false});
+
+            io.in(clientRooms[client]).emit("user_update", doc.value.user);
         });
 
         client.on("update_name", async name => {
-            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"user.name": name}}, {returnOriginal: false});
+            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"user.name": name.toString()}}, {returnOriginal: false});
 
-            io.emit("user_update", doc.value.user);
+            io.in(clientRooms[client]).emit("user_update", doc.value.user);
         });
 
-        client.on("update_notes", async notes => { //TODO validation
-            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"notes": notes}}, {returnOriginal: false});
+        client.on("remove_profile_picture", async () => {
+            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"user.profileImage": null}}, {returnOriginal: false});
 
-            io.emit("notes_update", doc.value.notes);
+            io.in(clientRooms[client]).emit("user_update", doc.value.user);
+        });
+
+        client.on("remove_background_image", async () => {
+            await findAndUpdateActiveDisplay(client, {"$set": {"backgroundUrl": null}}, {returnOriginal: false});
+
+            io.in(clientRooms[client]).emit("background_update", null);
+        });
+
+        client.on("update_widgets", async widgets => {
+            //validate widgets to ensure data is sanitized
+            let sanitizedWidgets = new Array(3);
+
+            if (!(widgets instanceof Array)) {
+                return;
+            }
+
+            for (let i = 0; i < 3; i++) {
+                sanitizedWidgets[i] = widgets[i] ? widgets[i].toString() : null;
+            }
+
+            //TODO filter to valid widget types here
+
+            await findAndUpdateActiveDisplay(client, {"$set": {"widgets": sanitizedWidgets}}, {returnOriginal: false});
+
+            io.in(clientRooms[client]).emit("widgets_update", sanitizedWidgets);
+        });
+
+        client.on("update_notes", async notes => {
+            //validate notes to ensure data is sanitized
+            let sanitizedNotes = [];
+
+            console.log(notes);
+
+            if (!(notes instanceof Array)) {
+                return;
+            }
+
+            notes.forEach(note => {
+                console.log(note);
+                if (!note.text && !note.image) {
+                    return; //invalid note! needs either text or image
+                }
+                sanitizedNotes.push({
+                    text: !note.text ? null : note.text.toString(),
+                    image: !note.image ? null : note.image.toString()
+                });
+            });
+
+            console.log("!!!!");
+            console.log(sanitizedNotes);
+
+            let doc = await findAndUpdateActiveDisplay(client, {"$set": {"notes": sanitizedNotes}}, {returnOriginal: false});
+
+            io.in(clientRooms[client]).emit("notes_update", doc.value.notes);
         })
 
-        client.on("update_calendar", async url => {
-            await findAndUpdateActiveDisplay(client, {"$set": {"calendarUrl": url}});
+        client.on("update_calendar", async calendars => {
+            let sanitizedCalendars = [];
 
-            if (url) {
-                let data = await getCalendarData(url);
-                io.emit("calendar_update", {url, data});
-            } else {
-                io.emit("calendar_update", null);
+            if (!(calendars instanceof Array)) {
+                return;
             }
+
+            calendars.forEach(cal => {
+                console.log("CAL:", cal);
+                if (!cal.url || !cal.colour) {
+                    return;
+                }
+                sanitizedCalendars.push({url: cal.url.toString(), colour: cal.colour.toString()});
+            });
+
+            await findAndUpdateActiveDisplay(client, {"$set": {"calendars": sanitizedCalendars}});
+
+            let eventData = await getAllCalendarData(calendars);
+
+            io.in(clientRooms[client]).emit("calendar_update", eventData);
+
+            /*if (url) {
+                let data = await getCalendarData(url);
+                io.in(clientRooms[client]).emit("calendar_update", {url, data});
+            } else {
+                io.in(clientRooms[client]).emit("calendar_update", null);
+            }*/
         });
 
         client.on("clear_messages", () => {
-            io.emit("clear_messages");
-        })
+            io.in(clientRooms[client]).emit("clear_messages");
+        });
+
+        client.on("delete_doodles", async () => {
+            await findAndUpdateActiveDisplay(client, {"$set": {"doodles": []}})
+
+            io.in(clientRooms[client]).emit("doodles_update", []);
+        });
     }
 
 //============== SOCKET.IO ==============
@@ -266,13 +407,13 @@ mongoClient.connect(databaseUrl).then(db => {
             delete clientRooms[client];
         });
 
-        client.on("join_room", async roomCode => {
+        client.on("join_room", async (roomCode, newPairing) => {
             let doc = await displays.findOne({"roomId": roomCode});
 
             if (doc) {
                 clientRooms[client] = roomCode;
 
-                registerClientEvents(client, true); //allow clients to use the rest of the system
+                registerClientEvents(client, true, newPairing); //allow clients to use the rest of the system
 
                 client.emit("room_joined", roomCode);
             } else {
@@ -304,6 +445,15 @@ function generateRoomId() {
     return Math.floor(1000000 + (Math.random() * 9000000)).toString();
 }
 
+async function getAllCalendarData(calendars) {
+
+    return await Promise.all(calendars.map(async c => new Object({
+        url: c.url,
+        events: await getCalendarData(c.url),
+        colour: c.colour
+    })));
+}
+
 async function getCalendarData(url) {
     let data = await fetch(url);
     data = await data.text();
@@ -313,8 +463,9 @@ async function getCalendarData(url) {
     return Object.keys(cal).map(k => cal[k]).filter(e => e.type === "VEVENT").map(e => {
         return {
             name: e.summary,
-            start: e.start.getTime(),
-            end: e.end.getTime()
+            description: e.description,
+            start: !e.start ? 0 : e.start.getTime(),
+            end: !e.end ? 0 : e.end.getTime()
         }
     });
 
